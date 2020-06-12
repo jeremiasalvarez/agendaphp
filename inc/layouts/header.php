1e4791125d43f966ec93c579317f103eca200e33
Jeremias Alvarez <jerealvarez34@gmail.com>
2020-06-12 02:29:16 -0300

    index html and css

--- inc/layouts/header.php
+++ inc/layouts/header.php
@@ -7,6 +7,7 @@
     <title>Agenda de Contactos</title>
     <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.0/normalize.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,10 +1,96 @@
 <?php include_once 'inc/layouts/header.php'; ?>
 
-    <div class="bar-container">
-        <h1>Agenda de Contactos</h1>
-    
-    </div>
+<div class="bar-container">
+    <h1>Agenda de Contactos</h1>
+</div>
 
+<div class="bg-yellow container shadow">
+
+    <form id="contacto" action="">
+        <legend>Añada un Contacto <span>Todos los campos son obligarios</span></legend>
+
+        <div class="fields">
+            <div class="field">
+                <label for="name">Nombre:</label>
+                <input type="text" placeholder="Nombre de Contacto"
+                id="name">
+            </div> 
+            <div class="field">
+                <label for="workplace">Empresa:</label>
+                <input type="text" placeholder="Nombre de Empresa"
+                id="workplace">
+            </div> 
+            <div class="field">
+                <label for="phone">Telefono:</label>
+                <input type="tel" placeholder="Telefono de Contacto"
+                id="phone">
+            </div>      
+        </div>
+        <div class="field send">
+                <input type="submit" value="Añadir">
+        </div>  
+    </form>
+
+</div>
+
+<div class="bg-white container shadow contacts">
+    <div class="contacts-container">
+        <h2>Contactos</h2>
+        <input type="text" id="search" class="explorer shadow" placeholder="Buscar Contactos..">
+
+        <p class="total-contacts"><span>2 </span>Contactos</p>
+
+        <div class="table-container">
+            <table id="contact-list" class="contact-list">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Empresa</th>
+                        <th>Telefono</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Juan</td>
+                        <td>Apple</td>
+                        <td>23424235</td>
+                        <td>
+                            <a class="btn btn-edit" href="#">
+                                <i class="fas fa-pen-square"></i>
+                            </a>
+                            <button data-id="1" class="btn btn-delete" type="button"><i class="fas fa-trash-alt"></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Juan</td>
+                        <td>Apple</td>
+                        <td>23424235</td>
+                        <td>
+                            <a class="btn btn-edit" href="#">
+                                <i class="fas fa-pen-square"></i>
+                            </a>
+                            <button data-id="1" class="btn btn-delete" type="button"><i class="fas fa-trash-alt"></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Juan</td>
+                        <td>Apple</td>
+                        <td>23424235</td>
+                        <td>
+                            <a class="btn btn-edit" href="#">
+                                <i class="fas fa-pen-square"></i>
+                            </a>
+                            <button data-id="1" class="btn btn-delete" type="button"><i class="fas fa-trash-alt"></i></button>
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
 
 
