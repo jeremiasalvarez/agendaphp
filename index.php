254148a64ed8f5aa8ab71723d50b45b82c46c76d
Jeremias Alvarez <jerealvarez34@gmail.com>
2020-06-12 00:28:16 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include_once 'inc/layouts/header.php'; ?>
+
+    <div class="bar-container">
+        <h1>Agenda de Contactos</h1>
+    
+    </div>
+
+
+
+
+<?php include_once 'inc/layouts/footer.php'; ?>
